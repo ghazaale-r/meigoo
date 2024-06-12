69c08054a5824be2b59f405191f7a74ae88d9aa7
ghazaale <qazale02@gmail.com>
2024-06-12 09:54:59 +0330

    Update views.py

--- src/restaurant/views.py
+++ src/restaurant/views.py
@@ -1,18 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from .models import Category
+from .models import Category, Restaurant
 
 
-# Create your views here.
-def test_home_page(request):
+# # Create your views here.
+# def test_home_page(request):
+#     # orm 
+#     categories = Category.objects.all()
+#     """select * form restaurant_Category"""
+#     context = {
+#         'food_part_available' : True,
+#         'categoriiis' : categories
+        
+#     }
+#     # return render(request, 'index.html')
+#     return render(request, 'restaurant/home_page.html', context)
+
+
+def restaurant_page_with_category(request, category_id=None):
+    
     
     categories = Category.objects.all()
-    context = {
-        'food_part_available' : True,
-        'categoriiis' : categories
+    
+    if not category_id:
+        restaurants = Restaurant.objects.all()
+    else:
+        categories_obj = Category.objects.get(id=category_id)
+        # categories_obj = get_object_or_404(Category, pk=category_id)
         
+        selected_category = categories_obj
+        restaurants = Restaurant.objects.filter(categories=categories_obj)
+        
+    context = {
+        'categories' : categories,
+        # 'restaurants' : restaurants,
+        'selected_category' : selected_category
     }
-    # return render(request, 'index.html')
-    return render(request, 'restaurant/home_page.html', context)
-
-
+    
+    return render(request, 'restaurant/restaurants.html', context)
