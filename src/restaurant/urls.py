cd4271f7740bd3cc9e9fce64029e9b41155b7838
ghazaale <qazale02@gmail.com>
2024-06-12 09:54:55 +0330

    Update urls.py

--- src/restaurant/urls.py
+++ src/restaurant/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import test_home_page
+from .views import restaurant_page_with_category
 
 urlpatterns = [
 
-    path('',  test_home_page),
-    # path('',  test_home_page),
+    # /category/1/restaurants
+    # /category/1
+    path('category/<int:category_id>/',  restaurant_page_with_category, name='category-restaurant'),
 ]
