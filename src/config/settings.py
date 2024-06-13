8debb0e0af6ae493ec1fd226ed0dea1e78896757
ghazaale <qazale02@gmail.com>
2024-06-14 01:11:19 +0330

    debug_toolbar added

--- src/config/settings.py
+++ src/config/settings.py
@@ -45,12 +45,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # third-party apps
+    'debug_toolbar',
     
     # custom apps
     'restaurant',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -142,3 +144,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
